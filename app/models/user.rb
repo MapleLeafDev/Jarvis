a47aeafb370fd07130a52b3987716b415882f5c8
Jason Isaac <jisaac@lightspeedsystems.com>
2012-11-09 13:45:12 -0800

    login working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.facebook_id = auth['uid']
+      user.name = auth['info']['name']
+      user.thumbnail = auth['info']['image']
+    end
+  end
 end
