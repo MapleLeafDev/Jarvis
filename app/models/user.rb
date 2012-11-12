fbcd72ef8a6d3ee99a9ecbd61360e498587c74ae
Jason Isaac <jisaac@lightspeedsystems.com>
2012-11-12 06:15:24 -0800

    user cards, some icons

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-
+  attr_accessible :name, :facebook_id, :thumbnail, :parent_id, :theme_id
+  
   def self.create_with_omniauth(auth)
     create! do |user|
       user.facebook_id = auth['uid']
