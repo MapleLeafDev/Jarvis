b6f62b8689b4535e69a432f124afa38a16ebb2db
Jason Isaac <jisaac@lightspeedsystems.com>
2012-11-15 17:39:46 -0800

    asdf

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -25,7 +25,7 @@ class CompletionsController < ApplicationController
   # GET /completions/new.json
   def new
     @completion = Completion.new
-    binding.pry
+    
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @completion }
