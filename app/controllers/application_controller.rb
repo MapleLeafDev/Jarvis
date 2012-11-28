e23069827818aea08b7a474c76b5dced35b7e55c
Jason Isaac <jisaac@lightspeedsystems.com>
2012-11-28 10:56:08 -0800

    fix for current user in testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,10 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    #@current_user ||= User.find(session[:user_id]) if session[:user_id]
-    @current_user = User.find(1)
+    if ENV['RAILS_ENV'] == 'development'
+      @current_user = User.find(1)
+    else
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
   end
 end
