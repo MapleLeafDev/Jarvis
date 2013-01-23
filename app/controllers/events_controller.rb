d061884b87ae62c14b6b0de9ba4afda3b080568f
Jason Isaac <jisaac@lightspeedsystems.com>
2013-01-22 20:01:23 -0800

    datepicker

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -39,7 +39,8 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(params[:event])
-binding.pry
+
+
     respond_to do |format|
       if @event.save
         format.html { redirect_to events_path, notice: 'Event was successfully created.' }
