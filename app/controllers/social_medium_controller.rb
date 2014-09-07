44a369db3682fc84ae19e557b92ef8583bcbb430
Jason Isaac <jisaac@lightspeedsystems.com>
2014-09-07 15:28:09 -0700

    mobile social media feed

--- app/controllers/social_medium_controller.rb
+++ app/controllers/social_medium_controller.rb
@@ -7,5 +7,10 @@ class SocialMediumController < ApplicationController
     when 1
       @results = @feed.instagram_recent_media
     end
+
+    respond_to do |format|
+      format.js 
+      format.js.phone { render template: "social_medium/show_m" }
+    end
   end
 end
