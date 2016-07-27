142bd5a975084011cf3a5ae1dcdba2965201e514
unknown <unknown@unknown.com>
2016-07-27 08:25:49 -0700

    remove sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,6 @@ HomeManager::Application.configure do
 
   config.eager_load = true
 
-  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
