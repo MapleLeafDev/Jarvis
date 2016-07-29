7584a7e0d8bb1955f7425ee93119a5e4e680f4f4
Jason <fleainabox@gmail.com>
2016-07-28 20:11:50 -0700

    google scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :tumblr, ENV['TUMBLR_ID'], ENV['TUMBLR_SECRET']
   provider :twitter, ENV['TWITTER_ID'], ENV['TWITTER_SECRET'], { :authorize_params => { :force_login => 'true' }}
-  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'], { :prompt => "select_account", :access_type => "offline" }
+  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'], { :scope => 'email, profile, plus.me', :prompt => "select_account", :access_type => "offline" }
 end
