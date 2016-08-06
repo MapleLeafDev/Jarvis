5c40a26e5707e824528eb4bdc201b972dfe36997
Jason <jasonpaulisaac@gmail.com>
2016-08-06 07:54:00 -0700

    force ssl

--- config/application.rb
+++ config/application.rb
@@ -57,13 +57,13 @@ module HomeManager
     config.assets.version = '1.0'
 
     config.action_mailer.smtp_settings = {
-        :address              => "smtp.gmail.com",
-        :port                 => 587,
-        :domain               => "gmail.com",
-        :user_name            => ENV['GMAIL_USERNAME'],
-        :password             => ENV['GMAIL_PASSWORD'],
-        :authentication       => :plain,
-        :enable_starttls_auto => true
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
     }
   end
 end
