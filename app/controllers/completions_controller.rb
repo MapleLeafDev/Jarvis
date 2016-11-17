ed2a3d180fb7f085a4c1d1bc58211a489d93857f
Jason <jasonpaulisaac@gmail.com>
2016-11-17 10:57:12 -0800

    completed fix

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -5,7 +5,8 @@ class CompletionsController < ApplicationController
     @task = Task.find_by_id(params[:task])
 
     if @task && !@task.completed?
-      Completion.create(user_id: current_user.id, task_id: @task.id, completed: Time.zone.today)
+      d = Time.now + Time.now.in_time_zone(current_user.timezone).utc_offset
+      Completion.create(user_id: current_user.id, task_id: @task.id, completed: "#{d.year}-#{d.month}-#{d.day}")
     end
   end
 
