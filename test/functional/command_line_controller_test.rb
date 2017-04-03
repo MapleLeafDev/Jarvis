86781076e6eb4584c9cc2e2c43202d6e5b86e579
Jason <jasonpaulisaac@gmail.com>
2017-04-03 10:27:11 -0700

    file cleanup

--- test/functional/command_line_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CommandLineControllerTest < ActionController::TestCase
-  test "should get run" do
-    get :run
-    assert_response :success
-  end
-
-end
--- test/unit/helpers/command_line_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CommandLineHelperTest < ActionView::TestCase
-end
